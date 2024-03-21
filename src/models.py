de59fd88949102c95cb1bf3cd5d438b20b6e146d
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-03-21 04:18:23 +0000

    starwarsApi

--- src/models.py
+++ src/models.py
@@ -7,6 +7,15 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'person'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    mail = Column(String(250), nullable=False)
+
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
